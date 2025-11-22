a2e4ce3cfa86100d037a5ff07bb2c6ed8e12a492
yigtuyumz <yigtuyumz@gmail.com>
2025-11-22 06:25:30 +0300

    demo

--- src/dlopen.h
+++ src/dlopen.h
@@ -1,4 +1,13 @@
 #ifndef DLOPEN_H
 # define DLOPEN_H
 
+
+# define NULL ((void *) 0)
+# define EXIT_FAILURE 1
+# define EXIT_SUCCESS 0
+
+typedef unsigned long size_t;
+
+# include <dlfcn.h>
+
 #endif /* DLOPEN_H */
--- src/main.c
+++ src/main.c
@@ -3,5 +3,28 @@
 int
 main(void)
 {
-    return (0);
+    // RTLD_NOW  -> resolve all symbols immediately
+    // RTDL_LAZY -> resolve symbol when needed
+    // loads libc
+    void *libc_handler = dlopen(NULL, RTLD_LAZY);
+
+    // why use stack!?
+    void *libc_symbols[3];
+
+    if (!libc_handler)
+        return (EXIT_FAILURE);
+    // here we can be more paranoid, but this thing aims to demontstrate the idea for now...
+    libc_symbols[0] = dlsym(libc_handler, "malloc");
+    libc_symbols[1] = dlsym(libc_handler, "printf");
+    libc_symbols[2] = dlsym(libc_handler, "free");
+
+    void *(*malloc)(size_t) = libc_symbols[0];
+    int (*printf)(const char *restrict, ...) = libc_symbols[1];
+    void (*free)(void *) = libc_symbols[2];
+
+    printf("Hello, World!\n%s\n", dlerror());
+    void *a = malloc(3);
+
+    free(a);
+    return (EXIT_SUCCESS);
 }
